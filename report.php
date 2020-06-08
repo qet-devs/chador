4b3c26b5fcf3cb53c711121f76d00f544ba8b33a
denisgmuriithi <denisg.muriithi@gmail.com>
2020-06-08 16:48:12 +0300

    reports update

--- report.php
+++ report.php
@@ -17338,7 +17338,14 @@ $aa=$i+1;
         <p><b>KANYI KIRUCHI & CO. ADVOCATES</b></p>
         <p><b>THIKA BUSINESS CENTRE, 5<sup>th</sup> FLOOR</b></p>
         <p><b>THIKA.</b></p>
+
+        <script type="text/javascript">
+window.print();
+window.onfocus=function(){ window.close();}
+</script>
+
 </div>
+
 
           <?php
         break;       
